c97f0d58135fb096e5c0c3b78ec6f2b1d4476d26
nataliasotelor <54275736+nataliasotelor@users.noreply.github.com>
2019-08-30 20:04:01 -0500

    Ejercicio 6 listo!

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,20 @@
-/*
+#include <iostream>
+#include<string>
+#include<vector>
 
- Leer tres numeros del usuario separados por espacio: 10 15 20
- Y encuentra el mayor valor: 30
+int main() {
+  int a,b,c;
+  using namespace std;
+  std::cout <<"Ingrese tres numeros separados por un espacio:";
 
- */
+  cin>>a>>b>>c;
+  if (a>b&&a>c){
+    cout<<"El mayor valor es:"<<a;
+  }
+  else if(b>a&&b>c){
+    cout<<"El mayor valor es:"<<b;
+  }
+  else{
+    cout<<"El mayor valor es:"<<c;
+  }
+}
